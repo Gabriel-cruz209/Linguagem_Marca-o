771bfbe996f7dd8dea529ff204b4c38112d3f160
Gabriel-cruz209 <gc572640@gmail.com>
2025-08-06 15:00:01 -0300

    Atualizado

---
+++ Jornal/Jornal.js
@@ -0,0 +1,16 @@
+
+function tecnologia() {
+    window.location.href = "JornalTec.html"
+}
+
+function politica() {
+    window.location.href = "JornalPoli.html"
+}
+
+function inicio() {
+    window.location.href = "Jornal.html"
+}
+
+function esporte() {
+    window.location.href = "JornalEsp.html"
+}
